d647768c2d09e0c02e62f1a037308dd58765a536
Tatsiana <tatsiana.zhv@gmail.com>
2018-09-18 16:34:18 +0300

    Default Changelist

--- homework4.php
+++ homework4.php
@@ -1,16 +1,22 @@
 <?php
-echo "<table border='1' cellspacing='0' cellpadding='3'>";
 
 $cols = 10;
 $rows = 10;
-$color = "#7375FF";
+$color1 = "#7375FF";
+$color2 = "FFE573";
+
+echo "<table border='1' cellspacing='0' cellpadding='3'>";
 
 for ($r=1; $r <= $rows ; $r++) {
     echo "<tr>";
     for ($c=1; $c <= $cols ; $c++) {
-        if (($r+$c)%11 ==0) {
-            echo "<td style='background-color: $color;'>".$r * $c."</td>";
-        } else {
+        if (($r-$c)%11==0) {
+            echo "<td style='background-color: $color1;'>".$r * $c."</td>";
+        }
+        elseif (($r+$c)%11==0){
+            echo "<td style='background-color: $color2;'>".$r * $c."</td>";
+        }
+        else {
             echo "<td>".$r * $c."</td>";
         }
 
