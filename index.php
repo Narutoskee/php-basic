95a56a1d0c491f4d3f3b053c4570b91783f944bb
Tatsiana <tatsiana.zhv@gmail.com>
2018-10-20 16:15:03 +0300

    Default Changelist

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+;
 
-phpinfo();
-die();
 include_once __DIR__."/config/db.php";
 include_once __DIR__."/include/functions.php";
 
