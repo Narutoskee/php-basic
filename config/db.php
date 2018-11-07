0ebb28cbdb26949d23f0acd130a10a175c6a9741
snabweb <narutoskee@gmail.com>
2018-11-07 18:39:52 +0300

    новый код

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'project');
-define('DB_USER', 'tatiana');
-define('DB_PASSWORD', '3456789');
+define('DB_USER', 'siriusblack');
+define('DB_PASSWORD', 'a167761A');
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
